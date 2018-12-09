d32429ed2ef3838208df95c464eea4d651cd2ef6
Allan Im <allan@vogle.com>
2018-12-09 12:05:16 -0500

    Initial Commit

---
+++ mad4114-xlambton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mad4114-xlambton
+//
+//  Created by Allan Im on 2018-12-09.
+//  Copyright © 2018 Allan Im. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
